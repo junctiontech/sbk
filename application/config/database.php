45449492257da6a218e141c2d7e650dcb4505a09
Rohit Singh Thakur <rohitjunctiontech>
2016-06-11 17:32:03 +0530

    d

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ if($_SERVER['HTTP_HOST']=="www.searchb4kharch.com"){
 	$password="initial1$";
 	$username="root";
 }
-if($_SERVER['HTTP_HOST']=="http://192.168.1.111/sbk/"){
+if($_SERVER['HTTP_HOST']=="http://192.168.1.111"){
 	$password="initial1$";
 	$username="root";
 }
