65ddc4a9624f02248553706f393fde0343ad5047
Rohit Singh Thakur <rohitjunctiontech>
2016-06-14 12:03:41 +0530

    h

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'www.searchb4kharch.com/';
 }elseif($_SERVER['HTTP_HOST']=="192.168.1.111"){
 	$config['base_url']	= 'http://192.168.1.111/sbk/';
 }
-print_r($config['base_url']);die;
+
 /*
 |--------------------------------------------------------------------------
 | Index File
