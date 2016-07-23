4c9a44922561362ed3d22de839f88ebe37b483e9
lekhpal <lekhpal@lekhpal-HP-Pavilion-15-Notebook-PC>
2016-07-23 15:41:58 +0530

    a

--- frontend/js/Filter_product.js
+++ frontend/js/Filter_product.js
@@ -1,14 +1,15 @@
 if(window.location.hostname=="localhost"){
 	var base_url = 'http://localhost/sbk/'; 
 }
-if(window.location.hostname=="junctiondev.cloudapp.net"){
-	var base_url = 'http://junctiondev.cloudapp.net/sbk/'; 
-}
+
 if(window.location.hostname=="searchb4kharch.com"){
 	var base_url = 'http://searchb4kharch.com/'; 
 }
 if(window.location.hostname=="www.searchb4kharch.com"){
 	var base_url = 'http://www.searchb4kharch.com/'; 
+}
+if(window.location.hostname=="192.168.1.151"){
+	var base_url = 'http://192.168.1.151/sbk/'; 
 }
 
 function Filter_product(value){ 
