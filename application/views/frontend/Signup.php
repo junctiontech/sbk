e87e06f7e672f61b2aaf624940b89f1edad3e620
Rohit Singh Thakur <rohitjunctiontech>
2016-07-30 13:28:37 +0530

    sd

--- application/views/frontend/Signup.php
+++ application/views/frontend/Signup.php
@@ -49,7 +49,7 @@
 								<label class="control-label col-md-3 col-sm-3 col-xs-12 form-label" for="name">Email<span class="required">*</span>
 								</label>
 								<div class="col-md-6 col-sm-6 col-xs-12">
-									<input id="name" class="form-control col-md-7 col-xs-12"  name="userEmail" placeholder="Last Name" required="required" type="text">
+									<input id="name" class="form-control col-md-7 col-xs-12"  name="userEmail" placeholder="Email" required="required" type="text">
 								</div>
 							</div>
 							 <div class="item form-group">
