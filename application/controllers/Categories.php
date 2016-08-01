b977fac5fb53af7d1c5edc3c3b835f3b1e868e09
Rohit Singh Thakur <rohitjunctiontech>
2016-08-01 12:24:38 +0530

    fds

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -202,7 +202,7 @@ class Categories extends CI_Controller
 	public function createfiltermodel($filtergroupID=false)
 	{
 		if(!empty($filtergroupID)){
-			$this->data['filtergroupdata']=$this->Categories_model->get_catfilter($categoriesID);
+			$this->data['filtergroupdata']=$this->Categories_model->get_catfilter($filtergroupID);
 		}
 		$this->data['categories']=$this->Categories_model->get_categorydata();
 		$this->load->view('admin/Createfiltermodel', $this->data);
