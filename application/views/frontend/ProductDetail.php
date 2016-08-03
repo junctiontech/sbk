4bd1e7f1c712693677a321d5b60b84b1ee9ba22b
Rohit Singh Thakur <rohitjunctiontech>
2016-08-03 10:48:31 +0530

    ds

--- application/views/frontend/ProductDetail.php
+++ application/views/frontend/ProductDetail.php
@@ -110,8 +110,8 @@
 								  data-clientid="1099405938736-82mgohcv3vc4cn0p8i1028vti6k0mpni.apps.googleusercontent.com"
 								  data-cookiepolicy="single_host_origin"
 								  data-prefilltext="Hey i found this great deal by searchb4kharch."
-								  data-calltoactionurl="http://<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>"
-								  data-calltoactiondeeplinkid="<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>">
+								  data-calltoactionurl="http://<?=$_SERVER['HTTP_HOST']?>/Landingpage/shareproduct/<?=$categoryval?>/<?=$sbkProductID?>/<?=$productkey?>.html"
+								  data-calltoactiondeeplinkid="http://<?=$_SERVER['HTTP_HOST']?>/Landingpage/shareproduct/<?=$categoryval?>/<?=$sbkProductID?>/<?=$productkey?>.html">
 								 <img
 								  src="<?=base_url();?>frontend/images/gplus.png" alt="Share on Google+"/>
 								</a></li>
