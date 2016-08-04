471d3ddf2f42b8f3bdb0b80cf2e45bcfabbed60b
Rohit Singh Thakur <rohitjunctiontech>
2016-08-04 10:52:31 +0530

    kj

--- application/views/frontend/ProductDetail.php
+++ application/views/frontend/ProductDetail.php
@@ -95,7 +95,7 @@
 					 
 					 <?php
                     $title=urlencode($products[0]->productName);
-                    $url=urlencode("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+                    $url=urlencode("http://".$_SERVER['HTTP_HOST'].'/Landingpage/shareproduct/'.$categoryval.'/'.$sbkProductID.'/'.$productkey.'.html');
                     $image=urlencode($products[0]->imageName);
 					$description=urlencode("hjkjasdkjaksjdk");
 					$caption=urlencode("dasd");
