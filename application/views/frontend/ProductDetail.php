53c2c8469f9540cd85de1e8b25e3ce4d508d3479
Rohit Singh Thakur <rohitjunctiontech>
2016-08-05 10:32:45 +0530

    sd

--- application/views/frontend/ProductDetail.php
+++ application/views/frontend/ProductDetail.php
@@ -96,7 +96,7 @@
 <span itemprop="name">hjj</span>
 <span itemprop="description">dsahsdj</span>
 <img itemprop="image" src="http://img.fkcdn.com/image/mobile/n/h/w/microsoft-lumia-950-xl-na-200x200-imaegf89yfs55ps5.jpeg">
-
+<link rel="image_src" href="http://img.fkcdn.com/image/mobile/n/h/w/microsoft-lumia-950-xl-na-200x200-imaegf89yfs55ps5.jpeg">
 					 <div class="share">
 						 <p>Share Product :</p>
 						 <ul>
