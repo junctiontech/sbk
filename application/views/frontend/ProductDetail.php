d0b44dfa75f64dbe45e052375afc1b935f1ad640
Rohit Singh Thakur <rohitjunctiontech>
2016-08-05 11:24:02 +0530

    asd

--- application/views/frontend/ProductDetail.php
+++ application/views/frontend/ProductDetail.php
@@ -1,8 +1,6 @@
  <div class="page-container">	
 	 <div class="main-content">	
-		 <div class="page-loading-overlay">			
-			 <div class="loader-2"><img src="<?=base_url();?>frontend/images/search-animated-icon.gif" style="width:200px;height:200px"></div>			
-		 </div>	 
+		 
 		 <div class="col-sm-12 col-md-12 col-xs-12 form_content ">
  <!-- Alert section For Message-->
 		 <?php  if($this->session->flashdata('message_type')=='success') {  ?>
